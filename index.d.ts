89344e88c5d0270fc5b09cf7c71753456758fb25
bakapear <proxybomb@gmail.com>
2021-12-21 21:13:50 +0100

    more param renaming

--- index.d.ts
+++ index.d.ts
@@ -4,8 +4,8 @@ export declare function resolve(value: string) : Promise<object>
 export declare function search(query: string, filters?: object): Promise<YoutubeSearch>
 export declare function video(videoId: string): Promise<YoutubeVideo>
 export declare function channel(channelId: string): Promise<YoutubeChannel>
-export declare function playlist(playlistId: string, index?: number): Promise<YoutubePlaylist>
+export declare function playlist(playlistId: string, startIndex?: number): Promise<YoutubePlaylist>
 export declare function comments(videoId: string, commentId?: string): Promise<YoutubeComment[]>
 export declare function download(videoId: string): Promise<YoutubeFormats>
 export declare function transcript(videoId: string): Promise<YoutubeTranscript>
-export declare function complete(value: string, lang?: string): Promise<string[]>
+export declare function complete(query: string, lang?: string): Promise<string[]>
--- src/complete.js
+++ src/complete.js
@@ -1,9 +1,9 @@
 let req = require('./lib/request')
 let URL = 'https://suggestqueries.google.com/complete/search'
 
-module.exports = async (value, lang = 'en') => {
+module.exports = async (query, lang = 'en') => {
   if (typeof id !== 'string') throw Error('Invalid value')
 
-  let body = await req.text(URL, { client: 'youtube', q: value, hl: lang, ds: 'yt', xssi: 't' })
+  let body = await req.text(URL, { client: 'youtube', q: query, hl: lang, ds: 'yt', xssi: 't' })
   return JSON.parse(body.slice(5))[1].map(x => x[0])
 }
