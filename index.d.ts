2c5d676e3c3cc4c8a72c19b40acba0bb0a513460
bakapear <proxybomb@gmail.com>
2024-03-07 21:40:55 +0100

    add searchfilters type

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,22 @@
 import { YouTubeSearch, YouTubeVideo, YouTubeChannel, YouTubePlaylist, YouTubeFormats, YouTubeTranscript } from './src/lib/structs.js'
 
+enum Period { 'hour', 'day', 'week', 'month', 'year' }
+enum Type { 'video', 'channel', 'playlist', 'movie' }
+enum Duration { 'short', 'long', 'medium' }
+enum Features { 'live', '4k', 'hd', 'subtitles', 'cc', '360', 'vr180', '3d', 'hdr', 'location', 'purchased' }
+enum Sort { 'relevance', 'rating', 'age', 'views' }
+
+interface SearchFilters {
+    period: keyof typeof Period
+    type: keyof typeof Type
+    duration: keyof typeof Duration
+    features: keyof typeof Features
+    sort: keyof typeof Sort
+}
+
+
 export declare function resolve(value: string) : Promise<object>
-export declare function search(query: string, filters?: object): Promise<YouTubeSearch>
+export declare function search(query: string, filters?: SearchFilters): Promise<YouTubeSearch>
 export declare function video(videoId: string): Promise<YouTubeVideo>
 export declare function channel(channelId: string): Promise<YouTubeChannel>
 export declare function playlist(playlistId: string, startIndex?: number): Promise<YouTubePlaylist>
