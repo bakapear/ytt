eb076ca0aaf0dea829b11fd1d2b4340a63f2ac04
bakapear <proxybomb@gmail.com>
2024-03-07 21:09:45 +0100

    fix syntax error

--- src/complete.js
+++ src/complete.js
@@ -2,7 +2,7 @@ import { text } from './lib/request.js'
 let URL = 'https://suggestqueries.google.com/complete/search'
 
 export default async (query, lang = 'en') => {
-  if (typeof id !== 'string') throw Error('Invalid value')
+  if (typeof query !== 'string') throw Error('Invalid value')
 
   let body = await text(URL, { client: 'youtube', q: query, hl: lang, ds: 'yt', xssi: 't' })
   return JSON.parse(body.slice(5))[1].map(x => x[0])
